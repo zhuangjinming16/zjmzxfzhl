c70317b63d8dde4038b1d707030f71aa283c582c
zjm16 <zjm16@163.com>
2019-10-23 22:11:38 +0800

    开发示例

---
+++ zjmzxfzhl-vue/src/views/demo/helloworld/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="app-container">
+        {{ msg }}
+    </div>
+</template>
+<script>
+    import {getAction} from '@/api/manage'
+    export default {
+        name: 'DemoHelloworld',//此处应与菜单表 T_SYS_MENU.ROUTE_NAME 值相同
+        data() {
+            return {
+                msg: ""
+            }
+        },
+        created() {
+            this.helloworld()
+        },
+        methods: {
+            helloworld() {
+                getAction('/demo/helloworld/helloworld').then((res) => {
+                    const {msg} = res
+                    this.msg = msg;
+                })
+            }
+        }
+    }
+</script>
