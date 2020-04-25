48f551ad5238056987d1baa3cf43b9f5eb97f016
庄金明 <zjm16@163.com>
2020-04-25 16:07:01 +0800

    前端、表结构、岗位管理

--- zjmzxfzhl/src/main/java/com/zjmzxfzhl/modules/sys/entity/SysPost.java
+++ zjmzxfzhl/src/main/java/com/zjmzxfzhl/modules/sys/entity/SysPost.java
@@ -1,11 +1,13 @@
 package com.zjmzxfzhl.modules.sys.entity;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.zjmzxfzhl.common.base.BaseEntity;
 import com.zjmzxfzhl.common.validator.constraints.LengthForUtf8;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,39 +21,33 @@ import lombok.Setter;
 @TableName("T_SYS_POST")
 public class SysPost extends BaseEntity {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 岗位ID
-	 */
-	@TableId
-	@NotNull
-	@LengthForUtf8(max = 32)
-	private String postId;
+    /**
+     * 岗位ID
+     */
+    @TableId
+    @NotNull
+    @LengthForUtf8(max = 32)
+    private String postId;
 
-	/**
-	 * 岗位名称
-	 */
-	@NotNull
-	@LengthForUtf8(max = 255)
-	private String postName;
+    /**
+     * 岗位名称
+     */
+    @NotNull
+    @LengthForUtf8(max = 255)
+    private String postName;
 
-	/**
-	 * 状态
-	 */
-	@LengthForUtf8(max = 1)
-	private String postStatus;
+    /**
+     * 排序号
+     */
+    @Max(9999)
+    private Integer sortNo;
 
-	/**
-	 * 排序号
-	 */
-	@Max(9999)
-	private Integer sortNo;
-
-	/**
-	 * 备注
-	 */
-	@LengthForUtf8(max = 255)
-	private String remark;
+    /**
+     * 备注
+     */
+    @LengthForUtf8(max = 255)
+    private String remark;
 
 }
