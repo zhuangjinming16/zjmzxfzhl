efd8f7097d67aba8b7a246a91168b43dc0d0932b
zjm16@163.com <zjm16@163.com>
2020-10-16 21:48:08 +0800

    delete zjmzxfzhl-common

--- zjmzxfzhl/zjmzxfzhl-common/zjmzxfzhl-common-core/src/main/java/com/zjmzxfzhl/common/core/base/BaseController.java
+++
@@ -1,8 +0,0 @@
-package com.zjmzxfzhl.common.core.base;
-
-/**
- * @author 庄金明
- * @date 2020年3月24日
- */
-public class BaseController {
-}
--- zjmzxfzhl/zjmzxfzhl-common/zjmzxfzhl-common-sys-api/src/main/java/com/zjmzxfzhl/modules/sys/service/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.zjmzxfzhl.modules.sys.service;
-
-import com.zjmzxfzhl.common.core.Result;
-import com.zjmzxfzhl.common.core.base.UserInfo;
-
-public interface UserInfoService {
-    Result<UserInfo> info(String userId, String inner);
-}
