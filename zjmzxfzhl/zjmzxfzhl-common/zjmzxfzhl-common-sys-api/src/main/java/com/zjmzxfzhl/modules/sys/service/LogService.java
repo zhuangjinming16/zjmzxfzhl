48f75a5a54a67184d596853fc23744be84274a39
zjm16@163.com <zjm16@163.com>
2020-10-16 22:07:04 +0800

    delete  zjmzxfzhl-common

--- zjmzxfzhl/zjmzxfzhl-common/zjmzxfzhl-common-sys-api/src/main/java/com/zjmzxfzhl/modules/sys/service/LogService.java
+++
@@ -1,8 +0,0 @@
-package com.zjmzxfzhl.modules.sys.service;
-
-import com.zjmzxfzhl.common.core.Result;
-import com.zjmzxfzhl.modules.sys.entity.SysLog;
-
-public interface LogService {
-    Result save(SysLog sysLog, String inner);
-}
